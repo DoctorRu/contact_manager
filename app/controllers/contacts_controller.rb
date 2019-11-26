f911a071b609d751ef83e1108e270365b2ac1f77
DoctorRu <vjblan@gmail.com>
2019-11-26 20:59:52 -0300

    update contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,45 @@
 class ContactsController < ApplicationController
-  def index
 
+  def index
     if params[:group_id] && !params[:group_id].empty?
-      @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
+      @contacts = Contact.where(group_id: params[:group_id]).order(created_at: :desc).page(params[:page])
     else
-      @contacts = Contact.page(params[:page])
+      @contacts = Contact.order(created_at: :desc).page(params[:page])
     end
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Contact was successfully created".
+      redirect to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(contact_params)
+      flash[:success] = "Contact was successfully updated"
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :group
 
+  validates :name, :email, :group_id, presence: true
+  
   def gravatar
     hash = Digest::MD5.hexdigest(email.downcase)
     "https://www.gravatar.com/avatar/#{hash}"
