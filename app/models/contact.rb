7edc9a9bb8b34daef75ae7724335e7cac26be799
DoctorRu <vjblan@gmail.com>
2019-12-10 19:12:11 -0300

    Implemented multi users support in the application

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -14,7 +14,7 @@ class Contact < ApplicationRecord
   end
 
   scope :search, -> (term) do 
-    where('LOWER(name) LIKE :term or LOWER(company) LIKE :term or LOWER(email) LIKE :term ', term: "%#{term.downcase}%") if     term.present? 
+    where('LOWER(name) LIKE :term or LOWER(company) LIKE :term or LOWER(email) LIKE :term ', term: "%#{term.downcase}%") if term.present? 
   end
 
   scope :by_group, -> (group_id) { where(group_id: group_id) if group_id.present? }
